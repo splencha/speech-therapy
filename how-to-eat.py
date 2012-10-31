e4ca94291594301b2445d2716fd5e4371a1d09d5
splencha <blinddonkeyhotai@gmail.com>
2012-10-31 12:00:59 -0400

    new how-to-eat code

---
+++ how-to-eat.py
@@ -0,0 +1,19 @@
+"""This branches the speech-therapy fork of speech-therapy (?I know...) so that I can work on the actual code in python/kivy to run the new tablet app. I will add more to the docs as I have more actual work done.
+"""
+
+def set_client():      #get client's name
+    #lots of code here
+    name = raw_input()
+    return name if len(name) >= 2 else set_client()
+
+
+
+
+
+
+
+
+
+
+
+
